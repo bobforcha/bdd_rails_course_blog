a543fa0d8b6233753d93369cd43ab33adc468587
Bob Forcha <bobforcha@gmail.com>
2018-10-11 23:07:16 -0400

    restrict access to edit action

--- spec/requests/articles_spec.rb
+++ spec/requests/articles_spec.rb
@@ -1,7 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe "Articles", type: :request do
-  let!(:article) { Article.create(title: "Title One", body: "Body of article one.") }
+  let!(:john)     { User.create(email: "john@example.com", password: "password") }
+  let!(:fred)     { User.create(email: "fred@example.com", password: "password") }
+  let!(:article)  { Article.create!(title: "Title One", body: "Body of article one.", user: john) }
 
   describe "GET /articles/:id" do
     context "with existing article" do
@@ -12,13 +14,51 @@ RSpec.describe "Articles", type: :request do
       end
     end
 
-    context "witjh non-existent article" do
+    context "with non-existent article" do
       before { get "/articles/xxxx" }
 
       it "handles non-existent article" do
         expect(response.status).to eq 302
         flash_message = "The article you're looking for couldn't be found"
         expect(flash[:alert]).to eq flash_message
+      end
+    end
+  end
+
+  describe "GET /articles/:id/edit" do
+    context "when user is not signed in" do
+      before { get "/articles/#{article.id}/edit" }
+
+      it "redirects to the sign-in page" do
+        expect(response.status).to eq 302
+        flash_message = "You need to sign in or sign up before continuing."
+        expect(flash[:alert]).to eq flash_message
+      end
+    end
+
+    context "when user is signed in" do
+      context "and is not the article's owner" do
+        before do
+          login_as fred
+          get "/articles/#{article.id}/edit"
+        end
+
+        it "redirects to the home page" do
+          expect(response.status).to eq 302
+          flash_message = "You can only edit your own articles."
+          expect(flash[:alert]).to eq flash_message
+        end
+      end
+
+      context "and is the article's owner" do
+        before do
+          login_as john
+          get "/articles/#{article.id}/edit"
+        end
+
+        it "renders the edit form" do
+          expect(response.status).to eq 200
+        end
       end
     end
   end
